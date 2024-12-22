cfb169b50e599ac85f1124f6a4fe8e16c0236b4d
v1605 <55302877+v1605@users.noreply.github.com>
2024-12-22 14:51:38 -0500

    Update main.ino to include mac and pid

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,16 @@
 #include "SnesController.h"
 #include <BleGamepad.h>
 
-///Pin Config
+///Pin and Mac Config
 int latchPin = 16;
 int clockPin = 17;
 int dataPin = 18;
+uint8_t newMACAddress[] = {0xA1, 0xB3, 0xC2, 0xD6, 0xEE, 0xFF - 0x02};
+uint16_t pidValue= 0xa4c0;
 ///
 
 SnesController controller;
-BleGamepad bleGamepad;
+BleGamepad bleGamepad("SNES Wireless Adapter", "v1605", 100);
 BleGamepadConfiguration bleGamepadConfig;
 void setup() {
   controller.init(latchPin, clockPin, dataPin);
@@ -20,7 +22,10 @@ void setup() {
   bleGamepadConfig.setAxesMin(0x0000);   
   bleGamepadConfig.setAxesMax(0x7FFF);  
   bleGamepadConfig.setAutoReport(false);
+  bleGamepadConfig.setVid(0xe5302);
+  bleGamepadConfig.setPid(pidValue);
   bleGamepad.begin(&bleGamepadConfig);
+  esp_base_mac_addr_set(&newMACAddress[0]);
 }
 
 void loop() {
@@ -31,4 +36,3 @@ void loop() {
     delay(1000);
   }
 }
-
